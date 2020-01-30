4dd0e6060af28c761811cf9319afb552c669d4dc
otman <calatmani@thenextclic.com>
2020-01-30 18:43:15 +0100

    feat(dev):seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
          $this->call(CitiesTableSeeder::class);
-//       $this->call(PartnersTableSeeder::class);
+         $this->call(PartnersTableSeeder::class);
 //       $this->call(DeliveryTimesTableSeeder::class);
 //       $this->call(CityDeliveryTimeTableSeeder::class);
 
